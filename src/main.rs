2e88af3592120f2c16531682a4fd82a1671e6fe1
Dan Oxlade <dan.oxlade@gmail.com>
2021-12-03 10:51:24 +0000

    working 3.a

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,44 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::str::FromStr;
 
-#[derive(Debug)]
-struct Position {
-    vertical: i32,
-    horizontal: i32,
-    aim: i32
-}
-
-impl Default for Position {
-    fn default() -> Self {
-        Self { vertical: 0, horizontal: 0, aim: 0 }
-    }
-}
-
-#[derive(Debug)]
-enum ParseInstructionError {
-    BadText
-}
-
-#[derive(Debug)]
-enum Instruction {
-    Forward(i32),
-    Up(i32),
-    Down(i32),
-}
-
-impl Position {
-    fn up(self, n: i32) -> Self {
-        return Position{ horizontal: self.horizontal, vertical: self.vertical, aim: self.aim - n}
-    }
-    fn down(self, n: i32) -> Self {
-        return Position{ horizontal: self.horizontal, vertical: self.vertical, aim: self.aim + n}
-    }
-    fn forward(self, n: i32) -> Self {
-        return Position{ horizontal: self.horizontal + n, vertical: self.vertical + self.aim * n, aim: self.aim}
-    }
-}
-
-impl FromStr for Instruction {
-    type Err = ParseInstructionError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts: Vec<_> = s.split(" ").into_iter().collect();
-        if parts.len() != 2 {
-            return Result::Err(ParseInstructionError::BadText);
-        }
-        let instruction = parts[0];
-        let quantity: i32 = parts[1].parse().map_err(|_| ParseInstructionError::BadText)?;
-        match instruction {            
-            "forward" => Result::Ok(Instruction::Forward(quantity)),
-            "up" => Result::Ok(Instruction::Up(quantity)),
-            "down" => Result::Ok(Instruction::Down(quantity)),
-            _ => Result::Err(ParseInstructionError::BadText)
-        }        
-    }
-}
+const WIDTH: usize = 12;
 
 fn main() -> io::Result<()>{
-    let lines = read_lines("./input/2.b.txt")?;
-    let it = lines
-        .map(|l| l.unwrap())
-        .map(|l| l.parse::<Instruction>().unwrap())
-        .fold(Position::default(), |accum, instruction| {
-            match instruction {
-                Instruction::Up(n) => accum.up(n),
-                Instruction::Down(n) => accum.down(n),
-                Instruction::Forward(n) => accum.forward(n)
-            }
-        });
+    let lines = read_lines("./input/3.a.txt")?;
+    
+    let mut bits: [i32; WIDTH] = [0; WIDTH];
+    let mut count: i32 = 0;
 
-    println!("result: {:?}", it.vertical * it.horizontal);
+    for line in lines {
+        let item = line.unwrap();
+        let split: Vec<_> = item
+            .split("")
+            .filter_map(|char| char.parse::<i32>().ok())
+            .collect();
+        for i in 0..WIDTH {            
+            bits[i] += split[i]
+        }
+        count += 1;
+    }
+    println!("bits: {:?}", bits);
+    println!("count: {:?}", count);
+    let hurdle = count / 2;
+    let mut g_total = 0;
+    let mut e_total = 0;
+
+    for i in 0..WIDTH {
+        let pos = (WIDTH-1) - i;
+        let bit = bits[pos];
+        if bit > hurdle {
+            g_total = g_total + 2_i32.pow(i.try_into().unwrap())
+        } else {
+            e_total = e_total + 2_i32.pow(i.try_into().unwrap())
+        };        
+    }
+
+    println!("result: g:{}, e:{} {:?}", g_total, e_total, g_total * e_total);
+
     Ok(())
 }
 
