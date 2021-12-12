3665d36b119d1523011ad62e01c46e090c7cb8ff
Dan Oxlade <dan.oxlade@gmail.com>
2021-12-12 20:14:30 +0000

    d12 pt 2 done

--- d12/main.rs
+++ d12/main.rs
@@ -1,9 +1,5 @@
-use std::{
-    collections::{HashMap, HashSet},
-    fmt::Debug,
-    hash::Hash,
-    str::FromStr,
-};
+use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn main() {
     part1();
@@ -11,21 +7,40 @@ fn main() {
 }
 
 fn part1() {
-    let input = include_str!("input.test.txt");
+    let input = include_str!("input.txt");
+    let connections = parse(input);
+    
+    let result = count(
+        "start".to_string(),
+        &connections,
+        &mut Vec::new(),
+    );
+    println!("part1: {:?}", result);
+}
 
+fn part2() {
+    pt2(include_str!("simple.test.txt"));
+    pt2(include_str!("slightly_larger.test.txt"));
+    pt2(include_str!("input.test.txt"));
+    pt2(include_str!("input.txt"));
+}
+
+fn parse(input: &str) -> HashMap<String, Vec<String>> {
     let mut connections: HashMap<String, Vec<String>> = HashMap::new();
     for line in input.lines() {
         let parts: Vec<_> = line.split("-").collect();
         let left = parts[0].to_string();
         let right = parts[1].to_string();
 
-        if let Some(it) = connections.get_mut(&left) {
-            it.push(right.clone());
-        } else {
-            let mut v = Vec::new();
-            v.push(right.clone());
-            connections.insert(left.clone(), v);
-        }
+        if right != "start" {
+            if let Some(it) = connections.get_mut(&left) {
+                it.push(right.clone());
+            } else {
+                let mut v = Vec::new();
+                v.push(right.clone());
+                connections.insert(left.clone(), v);
+            }
+        }        
 
         if left != "start" {
             if let Some(it) = connections.get_mut(&right) {
@@ -38,56 +53,73 @@ fn part1() {
         }
     }
 
-    println!("mappings: {:?}", connections);
-
-    let result = count(
-        "start".to_string(),
-        &connections,
-        &mut Vec::new(),
-        &mut HashMap::new(),
-    );
-    println!("part1: {:?}", result);
+    connections
 }
 
 fn count(
     curr: String,
     mappings: &HashMap<String, Vec<String>>,
     path: &mut Vec<String>,
-    memo: &mut HashMap<String, i32>,
 ) -> i32 {
     let is_lower = curr.to_lowercase() == curr;
 
     if is_lower && path.contains(&curr) {
-        println!("seen lower {:?},{}", path, curr);
         0
     } else {
         if curr == "end" {
-            println!("[{:?}] -> end", path);
             1
         } else {
             path.push(curr.clone());
-
-            if let Some(seen) = memo.get(&curr) {
-                println!("{:?} memo => {}", path, seen);
-                *seen
-            } else {
-
-                let children = mappings.get(&curr).unwrap();
-                let mut child_count = 0;
-                for child in children {
-                    let mut path_cp = path.clone();
-                    let c = count(child.clone(), mappings, &mut path_cp, memo);
-                    child_count += c;
-                    println!("child {:?},{} => {}", path_cp, child, c);
-                }
-                // memo.insert(curr.clone(), child_count);
-                println!("parent {:?} => {}", path, child_count);
-                child_count
+            let children = mappings.get(&curr).unwrap();
+            let mut child_count = 0;
+            for child in children {
+                let mut path_cp = path.clone();
+                let c = count(child.clone(), mappings, &mut path_cp);
+                child_count += c;
             }
+            child_count
         }
     }
 }
 
-fn part2() {
-    let input = include_str!("input.txt");
+fn pt2(input: &str) {
+    let connections = parse(input);
+    
+    let result = count2(
+        "start".to_string(),
+        &connections,
+        &mut Vec::new(),
+    );
+    println!("part2: {:?}", result);
+}
+
+fn count2(
+    curr: String,
+    mappings: &HashMap<String, Vec<String>>,
+    smalls: &mut Vec<String>,
+) -> i32 {
+    let is_lower = curr.to_lowercase() == curr;
+
+    let unique_small: HashSet<String> = HashSet::from_iter(smalls.clone());
+    let already_two = unique_small.len() < smalls.len();
+
+    if is_lower && already_two && unique_small.contains(&curr) { 
+        0
+    } else {
+        if curr == "end" {
+            1
+        } else {
+            if is_lower {
+                smalls.push(curr.clone());
+            }            
+            let children = mappings.get(&curr).unwrap();
+            let mut child_count = 0;
+            for child in children {
+                let mut path_cp = smalls.clone();
+                let c = count2(child.clone(), mappings, &mut path_cp);
+                child_count += c;
+            }
+            child_count
+        }
+    }
 }
