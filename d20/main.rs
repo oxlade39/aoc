b36672c79794ccb12b0462de1bb60f2fdc954bf7
Dan Oxlade <dan.oxlade@gmail.com>
2021-12-22 22:39:30 +0000

    working d20 pt2

--- d20/main.rs
+++ d20/main.rs
@@ -1,5 +1,5 @@
 use core::panic;
-use std::{time::Instant, collections::HashSet, hash::Hash, fmt::{Debug, Pointer}, iter::repeat};
+use std::{time::Instant, collections::HashSet, hash::Hash, fmt::Debug, iter::repeat};
 
 fn main() {
     let start = Instant::now();
@@ -28,6 +28,15 @@ fn part1() {
 }
 
 fn part2() {
+    let input = include_str!("input.txt");
+
+    let image_enhancement_algo = ImageEnhancementBits::new(input.lines().next().unwrap());
+    let input_image = InputImage::new( input.lines().skip(2).collect() );
+
+    let result = repeat(()).take(50)
+        .fold(input_image, |input, _| next(&input, &image_enhancement_algo));
+    println!("Part2 Image:\n{:?}", result);
+    println!("Part2:\n{:?}", result.light_pixels.len());
 }
 
 fn bin_to_i16(s: &str) -> i16 {
@@ -35,33 +44,44 @@ fn bin_to_i16(s: &str) -> i16 {
     i16::from_str_radix(s, 2).expect(&err)
 }
 
-fn bin_to_i64(s: &str) -> i64 {
-    i64::from_str_radix(s, 2).unwrap()
-}
-
-fn bin_to_usize(s: &str) -> usize {
-    usize::from_str_radix(s, 2).unwrap()
-}
-
 fn next(input_image: &InputImage, image_enhancement_algo: &ImageEnhancementBits) -> InputImage {
     let mut new_input_image: HashSet<Position> = HashSet::new();
 
-    for row in -2..(input_image.bottom() + 2) {
-        for col in -2..(input_image.right() + 2) {
-            let p = Position{ x: col, y: row };
-            let result = input_image.is_light(&p, &image_enhancement_algo);
-            if result {
-                new_input_image.insert(p);
+    // if current infinity is light then next infinity will be 512, else 0
+    let current_infinity_light = input_image.light_pixels.contains(&Position{ x: 0, y: 0 });
+    let next_infinity_light = if current_infinity_light {
+        image_enhancement_algo.max_light()
+    } else {
+        image_enhancement_algo.zero_light()
+    };
+
+    for row in 0..(input_image.height + 3) {
+        for col in 0..(input_image.width + 3) {
+            if row < 2 || col < 2 || row > input_image.height || col > input_image.width {
+                if next_infinity_light {
+                    let p = Position{ x: col, y: row };
+                    new_input_image.insert(p);
+                }
+            } else {
+                // shift pixels down and right each step
+                let p = Position{ x: col - 1, y: row - 1 };
+                let next_p = Position{ x: col, y: row };
+                let result = input_image.is_light(&p, &image_enhancement_algo);
+                if result {
+                    new_input_image.insert(next_p);
+                }
             }
         }
     }
 
-    InputImage{ light_pixels: new_input_image }
+    InputImage{ light_pixels: new_input_image, width: input_image.width + 2, height: input_image.height + 2 }
 }
 
 #[derive(Clone, PartialEq, Eq)]
 struct InputImage {
-    light_pixels: HashSet<Position>
+    light_pixels: HashSet<Position>,
+    width: i64,
+    height: i64,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -98,13 +118,15 @@ impl InputImage {
         for (row_num, row) in s.iter().enumerate() {
             for (col_num, c) in row.chars().enumerate() {
                 if c == '#' {
-                    let x = col_num as i64;
-                    let y = row_num as i64;
+                    let x = (col_num + 2) as i64;
+                    let y = (row_num + 2) as i64;
                     light_pixels.insert(Position{ x, y });
                 }
             }
         }
-        InputImage{ light_pixels }
+        let width = (s.iter().next().map(|line| line.len()).unwrap_or(0) + 4) as i64;
+        let height = (s.len() + 4) as i64;
+        InputImage{ light_pixels, width, height }
     }
 
     fn is_light(&self, p: &Position, iha: &ImageEnhancementBits) -> bool {
@@ -120,51 +142,26 @@ impl InputImage {
             "0"
         }
     }
-
-    fn bottom(&self) -> i64 {
-        self.light_pixels.iter().map(|p| p.y).max().unwrap()
-    }
-
-    
-    fn right(&self) -> i64 {
-        self.light_pixels.iter().map(|p| p.x).max().unwrap()
-    }
-
-    fn center(&self, p: &Position) -> Self {
-        let mut moved: HashSet<_> = HashSet::new();
-        for pos in self.light_pixels.iter() {
-            moved.insert(Position {
-                x: (pos.x - p.x),
-                y: (pos.y - p.y),
-            });
-        }
-
-        Self {
-            light_pixels: moved
-        }
-    }
-
-    fn re_center(&self) -> Self {
-        let min_x = self.light_pixels.iter().map(|p| p.x).min().unwrap();
-        let min_y = self.light_pixels.iter().map(|p| p.y).min().unwrap();
-        self.center(&Position{ x: min_x, y: min_y })
-    }
 }
 
 impl Debug for InputImage {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for i in 0..=self.bottom() {
-            for j in 0..=self.right() {
+        let mut result = Ok(());
+        for i in 0..self.height {
+            for j in 0..self.width {
                 let col = if self.light_pixels.contains(&Position{ x: j as i64, y: i as i64 }) {
                     "#"
                 } else {
                     "."
                 };
-                f.write_str(col);
+                result = f.write_str(col);
+                if result.is_err() {
+                    return result;
+                }
             }
-            f.write_str("\n");
+            result = f.write_str("\n");
         }
-        Ok(())
+        result
     }
 }
 
@@ -184,54 +181,14 @@ impl ImageEnhancementBits {
 
         ImageEnhancementBits(bits)
     }
-}
 
-#[test]
-fn test_center() {
-    let example = InputImage { light_pixels: HashSet::from_iter(vec![
-        Position{ x: 0, y: 0 }
-    ])};
+    fn zero_light(&self) -> bool {
+        self.0.contains(&0)
+    }
 
-    let result = example.center(&Position{ x: -1, y: -1 });
-    let expected = InputImage { light_pixels: HashSet::from_iter(vec![
-        Position{ x: 1, y: 1 }
-    ])};
-    assert_eq!(expected, result);
-}
-
-#[test]
-fn test_recenter() {
-    let example = InputImage { light_pixels: HashSet::from_iter(vec![
-        Position{ x: 1, y: 1 }
-    ])};
-
-    let result = example.re_center();
-    let expected = InputImage { light_pixels: HashSet::from_iter(vec![
-        Position{ x: 0, y: 0 }
-    ])};
-    println!("bottom: {:?}", expected.bottom());
-    println!("expected:\n{:?}", expected);
-    println!("result:\n{:?}", result);
-    assert_eq!(expected, result);
-}
-
-#[test]
-fn test_recenter_negative() {
-    let example = InputImage { light_pixels: HashSet::from_iter(vec![
-        Position{ x: -2, y: -2 },
-        Position{ x: 0, y: 0 },
-    ])};
-    println!("before:\n{:?}", example);
-
-    let result = example.re_center();
-    let expected = InputImage { light_pixels: HashSet::from_iter(vec![
-        Position{ x: 0, y: 0 },
-        Position{ x: 2, y: 2 },
-    ])};
-    println!("bottom: {:?}", expected.bottom());
-    println!("expected:\n{:?}", expected);
-    println!("result:\n{:?}", result);
-    assert_eq!(expected, result);
+    fn max_light(&self) -> bool {
+        self.0.contains(&511)
+    }
 }
 
 #[test]
@@ -239,6 +196,70 @@ fn test_bin() {
     let example = "000100010";
     let result = bin_to_i16(example);
     assert_eq!(34, result);
+}
+
+#[test]
+fn test_parse_input_image() {
+    let input = include_str!("input.test.txt");
+    let input_image = InputImage::new( input.lines().skip(2).collect() );
+
+    println!("input:\n{:?}", input_image);
+
+    assert_eq!(10, input_image.light_pixels.len());
+}
+
+#[test]
+fn test_is_light_false_zero() {
+    let ii = InputImage { 
+        light_pixels: HashSet::from_iter(vec![
+            Position{ x: 3, y: 3 }
+        ]),
+        height: 4,
+        width: 4,
+    };
+    let iha = ImageEnhancementBits(HashSet::new());
+
+    println!("ii:\n{:?}", ii);
+
+    assert_eq!(false, ii.is_light(&Position { x: 0, y: 0 }, &iha))
+}
+
+#[test]
+fn test_is_light_true_zero() {
+    let ii = InputImage { 
+        light_pixels: HashSet::from_iter(vec![]),
+        height: 3,
+        width: 3,
+    };
+    let iha = ImageEnhancementBits(HashSet::from_iter(vec![0]));
+
+    println!("ii:\n{:?}", ii);
+
+    assert_eq!(true, ii.is_light(&Position { x: 1, y: 1 }, &iha))
+}
+
+#[test]
+fn test_is_light_true_max() {
+    let ii = InputImage { 
+        light_pixels: HashSet::from_iter(vec![
+            Position{ x: 0, y: 0 },
+            Position{ x: 1, y: 0 },
+            Position{ x: 2, y: 0 },
+            Position{ x: 0, y: 1 },
+            Position{ x: 1, y: 1 },
+            Position{ x: 2, y: 1 },
+            Position{ x: 0, y: 2 },
+            Position{ x: 1, y: 2 },
+            Position{ x: 2, y: 2 },
+        ]),
+        height: 3,
+        width: 3,
+    };
+    let iha = ImageEnhancementBits(HashSet::from_iter(vec![511]));
+
+    println!("ii:\n{:?}", ii);
+
+    assert_eq!(true, ii.is_light(&Position { x: 1, y: 1 }, &iha))
 }
 
 #[test]
@@ -261,7 +282,6 @@ fn test_correct_count() {
         .fold(input_image, |input, _| next(&input, &image_enhancement_algo));
     println!("35:\n{:?}", result);
     assert_eq!(result.light_pixels.len(), 35);
-    println!("{:?}", result.light_pixels);
 
     input = include_str!("input.txt");
 
@@ -273,7 +293,7 @@ fn test_correct_count() {
     println!("5583:\n{:?}", result);
     assert_eq!(result.light_pixels.len(), 5583);
 
-    input = include_str!("input.txt");
+    input = include_str!("input.test.txt");
 
     image_enhancement_algo = ImageEnhancementBits::new(input.lines().next().unwrap());
     input_image = InputImage::new( input.lines().skip(2).collect() );
