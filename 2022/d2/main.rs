3c8b63838124c56fa52a44f7efedcd38f65b27f6
Dan Oxlade <dan.oxlade@gmail.com>
2022-12-20 21:47:24 +0000

    d2 p2

--- 2022/d2/main.rs
+++ 2022/d2/main.rs
@@ -5,6 +5,8 @@ fn main() {
     let input = include_str!("input.txt");
     let total: i32 = total_score(input);
     println!("total score: {total}");
+    let total_2: i32 = total_score_2(input);
+    println!("total score 2: {total_2}");
 }
 
 #[derive(Debug, PartialEq)]
@@ -17,11 +19,20 @@ enum HandShape {
 #[derive(Debug, PartialEq)]
 struct Round(HandShape, HandShape);
 
+struct RoundStrategy(HandShape, PlayResult);
+
 #[derive(Debug, PartialEq)]
 enum RoundParseError {
     BadLength,
     BadOpponent(String),
     BadOurs(String),
+}
+
+#[derive(Debug, PartialEq)]
+enum RoundStategyParseError {
+    BadLength,
+    BadOpponent(String),
+    BadResult(String),
 }
 
 impl FromStr for Round {
@@ -51,11 +62,56 @@ impl FromStr for Round {
     }
 }
 
+impl FromStr for RoundStrategy {
+    type Err = RoundStategyParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split(" ").collect();
+        if parts.len() != 2 {
+            return Err(RoundStategyParseError::BadLength);
+        }
+
+        let oppononent = match parts[0] {
+            "A" => Ok(HandShape::Rock),
+            "B" => Ok(HandShape::Paper),
+            "C" => Ok(HandShape::Sissors),
+            other => Err(RoundStategyParseError::BadOpponent(other.into()))
+        }?;
+
+        let pr = match parts[1] {
+            "X" => Ok(PlayResult::Lose),
+            "Y" => Ok(PlayResult::Draw),            
+            "Z" => Ok(PlayResult::Win),
+            other => Err(RoundStategyParseError::BadResult(other.into()))
+        }?;
+
+        Ok(RoundStrategy(oppononent, pr))
+    }
+}
+
 #[derive(Debug, PartialEq)]
 enum PlayResult {
     Draw,
     Win,
     Lose
+}
+
+impl RoundStrategy {
+    fn to_round(&self) -> Round {
+        match self {
+            RoundStrategy(HandShape::Paper, PlayResult::Draw) => Round(HandShape::Paper, HandShape::Paper),
+            RoundStrategy(HandShape::Paper, PlayResult::Lose) => Round(HandShape::Paper, HandShape::Rock),
+            RoundStrategy(HandShape::Paper, PlayResult::Win) => Round(HandShape::Paper, HandShape::Sissors),
+
+            RoundStrategy(HandShape::Rock, PlayResult::Draw) => Round(HandShape::Rock, HandShape::Rock),
+            RoundStrategy(HandShape::Rock, PlayResult::Lose) => Round(HandShape::Rock, HandShape::Sissors),
+            RoundStrategy(HandShape::Rock, PlayResult::Win) => Round(HandShape::Rock, HandShape::Paper),
+
+            RoundStrategy(HandShape::Sissors, PlayResult::Draw) => Round(HandShape::Sissors, HandShape::Sissors),
+            RoundStrategy(HandShape::Sissors, PlayResult::Lose) => Round(HandShape::Sissors, HandShape::Paper),
+            RoundStrategy(HandShape::Sissors, PlayResult::Win) => Round(HandShape::Sissors, HandShape::Rock)
+        }
+    }
 }
 
 impl Round {
@@ -98,6 +154,15 @@ fn total_score(input: &str) -> i32 {
     .sum()
 }
 
+fn total_score_2(input: &str) -> i32 {
+    input
+    .lines()
+    .map(|l| l.parse::<RoundStrategy>().unwrap())
+    .map(|rs| rs.to_round())
+    .map(|r| r.score())
+    .sum()
+}
+
 #[test]
 fn test_parse_round() {
     let line = "A Y";
@@ -130,4 +195,9 @@ fn test_play_scissors() {
 #[test]
 fn test_total_score_example() {
     assert_eq!(15, total_score(include_str!("input.example.txt")))
+}
+
+#[test]
+fn test_total_score2_example() {
+    assert_eq!(12, total_score_2(include_str!("input.example.txt")))
 }
