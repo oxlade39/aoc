ae51eb07f092e28e28bac83fbbad37997a6fc992
Dan Oxlade <dan.oxlade@gmail.com>
2023-12-02 12:03:01 +0000

    2023 d2

--- 2023/d1/main.rs
+++ 2023/d1/main.rs
@@ -1,8 +1,11 @@
+use std::{collections::{BinaryHeap, BTreeMap}, cmp::Reverse};
+
 use itertools::Itertools;
 
 fn main() {
     let input = include_str!("input.txt");
     println!("part1: {}", part1(input));
+    println!("part2: {}", part2(input));
 }
 
 fn part1(txt: &str) -> i32 {
@@ -13,11 +16,55 @@ fn part1(txt: &str) -> i32 {
             .filter(|c| c.to_digit(RADIX).is_some())
             .collect_vec()
         )
-        .map(|l| format!("{}{}", l.first().unwrap(), l.last().unwrap()))
+        .map(|l| {
+            // if (l.len() < 2) {
+                // println!("bad: {:?}", l);
+            // }
+            format!("{}{}", l.first().unwrap(), l.last().unwrap())
+        })
         .map(|i| i.parse::<i32>().unwrap())
         .sum()
 }
 
+fn part2(txt: &str) -> i32 {
+    let n = [
+        ["one", "1"],
+        ["two", "2"],
+        ["three", "3"],
+        ["four", "4"],
+        ["five", "5"],
+        ["six", "6"],
+        ["seven", "7"],
+        ["eight", "8"],
+        ["nine", "9"],
+    ];
+
+    let mut sum = 0;
+
+    for line in txt.lines() {
+        let mut forward = BTreeMap::new();
+        let mut back = BTreeMap::new();
+
+        for (n ,[word, num]) in n.iter().enumerate() {
+            if let Some(i) = line.find(word) {
+                forward.insert(i, n + 1);
+            }
+            if let Some(i) = line.find(num) {
+                forward.insert(i, n + 1);
+            }
+            if let Some(i) = line.rfind(word) {
+                back.insert(i, n + 1);
+            }
+            if let Some(i) = line.rfind(num) {
+                back.insert(i, n + 1);
+            }
+        }
+
+        sum += forward.pop_first().expect(line).1 * 10;
+        sum += back.pop_last().expect(line).1;
+    }
+    sum as i32
+}
 
 #[cfg(test)]
 mod tests {
@@ -28,5 +75,11 @@ mod tests {
     fn sample_input_pt1() {
         let input = include_str!("input.test.txt");
         assert_eq!(142, part1(input));
+    }
+
+    #[test]
+    fn sample_input_pt2() {
+        let input = include_str!("input2.test.txt");
+        assert_eq!(281, part2(input));
     }
 }
---
+++ 2023/d2/main.rs
@@ -0,0 +1,113 @@
+use std::{collections::HashMap, str::FromStr};
+
+use itertools::Itertools;
+
+fn main() {
+    let input = include_str!("input.txt");
+    println!("part1: {}", part1(input));
+    println!("part2: {}", part2(input));
+}
+
+fn part1(txt: &str) -> i32 {
+    let bag = {
+        let mut cubes = HashMap::new();
+        cubes.insert("red".to_owned(), 12);
+        cubes.insert("green".to_owned(), 13);
+        cubes.insert("blue".to_owned(), 14);
+        Bag { cubes }
+    };
+    
+    txt.lines()
+        .map(|l| l.parse::<Game>().expect(l))
+        .enumerate()
+        .filter(|(_i, g)| g.draws.iter().all(|d| bag.possible(d)))
+        .map(|(i, _)| (i + 1) as i32)
+        .sum()
+}
+
+fn part2(txt: &str) -> i32 {
+    txt.lines()
+        .map(|l| l.parse::<Game>().expect(l))
+        .map(|g| g.power())
+        .sum()
+}
+
+#[derive(Debug, Clone)]
+struct Game {
+    draws: Vec<Draw>
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Draw {
+    cubes: HashMap<String, i32>
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Bag {
+    cubes: HashMap<String, i32>
+}
+
+impl FromStr for Game {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let outter = s.split(": ").nth(1).expect("': '");
+        let inner = outter.split("; ")
+            .map(|draw| {
+                let cube_counts: HashMap<String, i32> = draw.split(", ")
+                    .map(|cc| {
+                        let parts = cc.split(" ").collect_vec();
+                        (parts[1].to_owned(), parts[0].parse::<i32>().expect("count"))
+                    })
+                    .collect();
+                Draw { cubes: cube_counts }
+            })
+            .collect_vec();
+
+        Ok(Game { draws: inner })
+    }
+}
+
+impl Game {
+    fn power(&self) -> i32 {
+        let mut maxs: HashMap<String, i32> = HashMap::new();
+        for d in &self.draws {
+            for (k, v) in &d.cubes {
+                if let Some(existing) = maxs.remove(k) {
+                    maxs.insert(k.clone(), (*v).max(existing));
+                } else {
+                    maxs.insert(k.clone(), *v);
+                }
+            }
+        }
+        maxs.values().product()
+    }
+}
+
+impl Bag {
+    fn possible(&self, draw: &Draw) -> bool {
+        for (k, v) in &draw.cubes {
+            if let Some(count) = self.cubes.get(k) {
+                if *v > *count {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn test_example_p1() {
+        assert_eq!(8, part1(include_str!("input.test.txt")));
+    }
+
+    #[test]
+    fn test_example_p2() {
+        assert_eq!(2286, part2(include_str!("input.test.txt")));
+    }
+}
