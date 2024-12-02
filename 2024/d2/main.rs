9abbe56290b20f7340f20308a006bf04adb195b7
Dan Oxlade <dan.oxlade@gmail.com>
2024-12-02 17:32:45 +0000

    2024 d2 pt2

--- 2024/d2/main.rs
+++ 2024/d2/main.rs
@@ -1,4 +1,4 @@
-use std::{iter::zip, time::Instant};
+use std::{iter::zip, str::FromStr, time::Instant};
 
 use aoclib::grid::{Grid, GridPosition};
 use itertools::Itertools;
@@ -15,54 +15,158 @@ fn main() {
 }
 
 fn part1(txt: &str) -> usize {
-    let valid = txt.lines().filter_map(|l| {
-        l.split_whitespace().fold(Some(RowState::new()), |accum, rs| {
-            let n: i32 = rs.parse().unwrap();
-            match accum {
-                Some(RowState { prev, prev_delta, }) => {
-                    match (prev, prev_delta) {
-                        (Some(p), Some(pd)) => {
-                            let diff = n - p;
+    solve(txt, 0)
+}
 
-                            if diff.signum() != pd.signum() || diff.abs() < 1 || diff.abs()> 3 {
-                                return None;
-                            }
-        
-                            Some(RowState { prev: Some(n), prev_delta: Some(diff) })
-                        },
-                        (Some(p), None) => {
-                            let diff = n - p;
-                            if diff.abs() < 1 || diff.abs() > 3 {
-                                return None;
-                            }
-        
-                            Some(RowState { prev: Some(n), prev_delta: Some(diff) })
-                        }
-                        (None, None) => Some(RowState { prev: Some(n), prev_delta: None }),
-                        (None, Some(_)) => panic!("impossible"),                        
-                    }                    
-                },
-                None => None,
-            }
-        })
-    });
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Report {
+    levels: Vec<usize>,
+}
 
-    valid.count()
+impl FromStr for Report {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let levels = s
+            .split_whitespace()
+            .map(|n| n.parse::<usize>().unwrap())
+            .collect();
+        Ok(Report { levels })
+    }
+}
+
+trait SafetyReport {
+    fn is_safe(&self) -> bool;
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Part1(Report);
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Part2(Report);
+
+impl SafetyReport for Part1 {
+    fn is_safe(&self) -> bool {
+        all_safe(&self.0.levels)
+    }
+}
+
+impl SafetyReport for Part2 {
+    fn is_safe(&self) -> bool {
+        any_safe(&self.0)
+    }
+}
+
+fn all_safe(r: &Vec<usize>) -> bool {
+    let all_increasing = r.iter().copied()
+        .tuple_windows()
+        .all(|(left, right)| (right as i64 - left as i64).signum() == 1);
+    let all_decreasing = r.iter().copied()
+        .tuple_windows()
+        .all(|(left, right)| (right as i64 - left as i64).signum() == -1);
+    let all_in_range = r.iter().copied()
+        .tuple_windows()
+        .all(|(left, right)| (left as i64).abs_diff(right as i64) > 0 && (left as i64).abs_diff(right as i64) <= 3);
+
+    (all_increasing || all_decreasing) && all_in_range
+}
+
+fn any_safe(r: &Report) -> bool {
+    (0..r.levels.len()).any(|i| {
+        let mut without = r.levels.clone();
+        without.remove(i);
+        all_safe(&without)
+    })
 }
 
 fn part2(txt: &str) -> usize {
-    0
+    txt.lines()
+        .map(|l| l.parse::<Report>().unwrap())
+        .map(|report| Part2(report))
+        .filter(|pt| pt.is_safe())
+        .count()
+}
+
+fn solve(txt: &str, allowed_n: i32) -> usize {
+    let valid = txt.lines().filter_map(|l| {
+        l.split_whitespace()
+            .fold(Some(RowState::new(allowed_n)), |accum, rs| {
+                let n: i32 = rs.parse().unwrap();
+                match accum {
+                    Some(RowState {
+                        prev,
+                        prev_delta,
+                        allowed,
+                    }) => match (prev, prev_delta) {
+                        (Some(p), Some(pd)) => {
+                            let diff = n - p;
+
+                            if diff.signum() != pd.signum() || diff.abs() < 1 || diff.abs() > 3 {
+                                if allowed == 0 {
+                                    return None;
+                                }
+                                return Some(RowState {
+                                    prev,
+                                    prev_delta,
+                                    allowed: allowed - 1,
+                                });
+                            }
+
+                            Some(RowState {
+                                prev: Some(n),
+                                prev_delta: Some(diff),
+                                allowed,
+                            })
+                        }
+                        (Some(p), None) => {
+                            let diff = n - p;
+                            if diff.abs() < 1 || diff.abs() > 3 {
+                                if allowed == 0 {
+                                    return None;
+                                }
+
+                                return Some(RowState {
+                                    prev,
+                                    prev_delta,
+                                    allowed: allowed - 1,
+                                });
+                            }
+
+                            Some(RowState {
+                                prev: Some(n),
+                                prev_delta: Some(diff),
+                                allowed,
+                            })
+                        }
+                        (None, None) => Some(RowState {
+                            prev: Some(n),
+                            prev_delta: None,
+                            allowed,
+                        }),
+                        (None, Some(_)) => panic!("impossible"),
+                    },
+                    None => None,
+                }
+            })
+    });
+
+    valid.count()
 }
 
 #[derive(Debug)]
 struct RowState {
     prev: Option<i32>,
     prev_delta: Option<i32>,
+    allowed: i32,
 }
 
 impl RowState {
-    fn new() -> Self {
-        RowState { prev: None, prev_delta: None }
+    fn new(allowed: i32) -> Self {
+        RowState {
+            prev: None,
+            prev_delta: None,
+            allowed,
+        }
     }
 }
 
@@ -74,7 +178,34 @@ mod tests {
     fn sample_input_pt1_lines() {
         assert_eq!(1, part1("7 6 4 2 1"));
     }
-    
+
+    #[test]
+    fn sample_input2_pt2() {
+        // 7 6 4 2 1: Safe without removing any level.
+        let report = "7 6 4 2 1".parse::<Report>().unwrap();
+        assert_eq!(true, Part2(report).is_safe());
+
+        // 1 2 7 8 9: Unsafe regardless of which level is removed.
+        let report = "1 2 7 8 9".parse::<Report>().unwrap();
+        assert_eq!(false, Part2(report).is_safe());
+
+        // 9 7 6 2 1: Unsafe regardless of which level is removed.
+        let report = "9 7 6 2 1".parse::<Report>().unwrap();
+        assert_eq!(false, Part2(report).is_safe());
+
+        // 1 3 2 4 5: Safe by removing the second level, 3.
+        let report = "1 3 2 4 5".parse::<Report>().unwrap();
+        assert_eq!(true, Part2(report).is_safe());
+
+        // 8 6 4 4 1: Safe by removing the third level, 4.
+        let report = "8 6 4 4 1".parse::<Report>().unwrap();
+        assert_eq!(true, Part2(report).is_safe());
+
+        // 1 3 6 7 9: Safe without removing any level.
+        let report = "1 3 6 7 9".parse::<Report>().unwrap();
+        assert_eq!(true, Part2(report).is_safe());
+    }
+
     #[test]
     fn sample_input_pt1() {
         let test_input = include_str!("input.test.txt");
@@ -84,18 +215,18 @@ mod tests {
     #[test]
     fn input_pt1() {
         let test_input = include_str!("input.txt");
-        assert_eq!(0, part1(test_input));
+        assert_eq!(246, part1(test_input));
     }
 
     #[test]
     fn test_input_pt2() {
         let test_input = include_str!("input.test.txt");
-        assert_eq!(0, part2(test_input));
+        assert_eq!(4, part2(test_input));
     }
 
     #[test]
     fn input_pt2() {
         let test_input = include_str!("input.txt");
-        assert_eq!(0, part2(test_input));
+        assert_eq!(318, part2(test_input));
     }
 }
