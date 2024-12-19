683556134026516c445b98e44cc226cbbf30ca00
Dan Oxlade <dan.oxlade@gmail.com>
2024-12-19 13:35:04 +0000

    tidy

--- 2024/d19/main.rs
+++ 2024/d19/main.rs
@@ -1,8 +1,8 @@
 use core::str;
-use std::{collections::BinaryHeap, i64, str::FromStr, time::Instant, usize};
+use std::{i64, time::Instant, usize};
 
 use aoclib::{input, timing};
-use hashbrown::{HashMap, HashSet};
+use hashbrown::HashMap;
 use itertools::Itertools;
 
 fn main() {
@@ -17,42 +17,42 @@ fn part1(txt: &str) -> i64 {
     let (towels, to_check) = input::empty_line_chunks(txt).tuples().next().unwrap();
     let towels = towels.split(", ").map(|p| TowelPattern(p.to_owned()));
     
-    let mut patterns: HashMap<char, BinaryHeap<TowelPattern>> = HashMap::new();
+    let mut patterns: HashMap<char, Vec<TowelPattern>> = HashMap::new();
 
     for t in towels {
         let fc = t.0.chars().next().unwrap();
         if let Some(existing) = patterns.get_mut(&fc) {
             existing.push(t);
         } else {
-            let mut bh = BinaryHeap::new();
+            let mut bh = Vec::new();
             bh.push(t);
             patterns.insert(fc, bh);
         }
     }
 
-    let mut count = 0;
+    let mut c = 0;
     let to_check: Vec<_> = to_check.lines().collect();
 
     for item in to_check {
-        if find(item, 0, &mut patterns, &mut HashSet::new()) {
-            count += 1;
+        if count(item, 0, &mut patterns, &mut HashMap::new()) > 0 {
+            c += 1;
         }
     }
-    count
+    c
 }
 
 fn part2(txt: &str) -> usize {
     let (towels, to_check) = input::empty_line_chunks(txt).tuples().next().unwrap();
     let towels = towels.split(", ").map(|p| TowelPattern(p.to_owned()));
     
-    let mut patterns: HashMap<char, BinaryHeap<TowelPattern>> = HashMap::new();
+    let mut patterns: HashMap<char, Vec<TowelPattern>> = HashMap::new();
 
     for t in towels {
         let fc = t.0.chars().next().unwrap();
         if let Some(existing) = patterns.get_mut(&fc) {
             existing.push(t);
         } else {
-            let mut bh = BinaryHeap::new();
+            let mut bh = Vec::new();
             bh.push(t);
             patterns.insert(fc, bh);
         }
@@ -67,48 +67,10 @@ fn part2(txt: &str) -> usize {
     c
 }
 
-fn find(
-    s: &str,
-    position: usize,
-    patterns: &mut HashMap<char, BinaryHeap<TowelPattern>>,
-    seen: &mut HashSet<String>,
-) -> bool {
-    let sub = &s[position..];
-    if sub.is_empty() {
-        return true;
-    }
-    if seen.contains(sub) {
-        return false;
-    }
-    let c = sub.chars().next().expect(&format!("no char in {:?}", sub));
-    if let Some(potential_children) = patterns.get_mut(&c) {
-
-        let mut clone = potential_children.clone();
-        while let Some(next) = clone.pop() {
-            if next.0.len() > sub.len() {
-                continue;
-            }
-
-            let sub_sub = &sub[0..next.0.len()];
-            if sub_sub == next.0 {
-                // try match recursive
-                let matched_pos = position + next.0.len();
-                let _rhs = &s[matched_pos..];
-                if find(s, matched_pos, patterns, seen) {                    
-                    seen.insert(sub_sub.to_owned());
-                    return true;
-                }
-            }
-        }
-    }
-    seen.insert(sub.to_owned());
-    false
-}
-
 fn count(
     s: &str,
     position: usize,
-    patterns: &HashMap<char, BinaryHeap<TowelPattern>>,
+    patterns: &HashMap<char, Vec<TowelPattern>>,
     memo: &mut HashMap<String, usize>,
 ) -> usize {
 
@@ -145,24 +107,8 @@ fn count(
     0
 }
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug)]
 struct TowelPattern(String);
-
-impl PartialOrd for TowelPattern {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        let my_len = self.0.len();
-        let other_len = other.0.len();
-        my_len.partial_cmp(&other_len)
-    }
-}
-
-impl Ord for TowelPattern {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        let my_len = self.0.len();
-        let other_len = other.0.len();
-        my_len.cmp(&other_len)
-    }
-}
 
 #[cfg(test)]
 mod tests {    
@@ -170,36 +116,36 @@ mod tests {
 
     #[test]
     fn test_find() {
-        let mut patterns: HashMap<char, BinaryHeap<TowelPattern>> = HashMap::new();
-        patterns.insert('r', BinaryHeap::from_iter(vec![TowelPattern("r".to_owned())]));
-        assert_eq!(true, find("r", 0, &mut patterns, &mut HashSet::new()));
+        let mut patterns: HashMap<char, Vec<TowelPattern>> = HashMap::new();
+        patterns.insert('r', vec![TowelPattern("r".to_owned())]);
+        assert_eq!(true, count("r", 0, &mut patterns, &mut HashMap::new()) > 0);
 
-        let mut patterns: HashMap<char, BinaryHeap<TowelPattern>> = HashMap::new();
-        patterns.insert('r', BinaryHeap::from_iter(vec![TowelPattern("rw".to_owned())]));
-        assert_eq!(true, find("rw", 0, &mut patterns, &mut HashSet::new()));
+        let mut patterns: HashMap<char, Vec<TowelPattern>> = HashMap::new();
+        patterns.insert('r', vec![TowelPattern("rw".to_owned())]);
+        assert_eq!(true, count("rw", 0, &mut patterns, &mut HashMap::new()) > 0);
 
-        let mut patterns: HashMap<char, BinaryHeap<TowelPattern>> = HashMap::new();
-        patterns.insert('r', BinaryHeap::from_iter(vec![
+        let mut patterns: HashMap<char, Vec<TowelPattern>> = HashMap::new();
+        patterns.insert('r', vec![
             TowelPattern("r".to_owned()),
             TowelPattern("rw".to_owned()),
-        ]));
-        assert_eq!(true, find("rw", 0, &mut patterns, &mut HashSet::new()));
-        assert_eq!(true, find("rrw", 0, &mut patterns, &mut HashSet::new()));
+        ]);
+        assert_eq!(true, count("rw", 0, &mut patterns, &mut HashMap::new()) > 0);
+        assert_eq!(true, count("rrw", 0, &mut patterns, &mut HashMap::new()) > 0);
 
     }
 
     #[test]
     fn test_find_greedy() {
         // greedy
-        let mut patterns: HashMap<char, BinaryHeap<TowelPattern>> = HashMap::new();
-        patterns.insert('r', BinaryHeap::from_iter(vec![
+        let mut patterns: HashMap<char, Vec<TowelPattern>> = HashMap::new();
+        patterns.insert('r', vec![
             TowelPattern("rw".to_owned()),
             TowelPattern("rww".to_owned()),            
-        ]));
-        patterns.insert('w', BinaryHeap::from_iter(vec![
+        ]);
+        patterns.insert('w', vec![
             TowelPattern("wr".to_owned()),
-        ]));
-        assert_eq!(true, find("rwwr", 0, &mut patterns, &mut HashSet::new()));
+        ]);
+        assert_eq!(true, count("rwwr", 0, &mut patterns, &mut HashMap::new()) > 0);
     }
 
     #[test]
@@ -216,18 +162,18 @@ mod tests {
 
     #[test]
     fn test_count() {
-        let mut patterns: HashMap<char, BinaryHeap<TowelPattern>> = HashMap::new();
-        patterns.insert('r', BinaryHeap::from_iter(vec![TowelPattern("r".to_owned())]));
+        let mut patterns: HashMap<char, Vec<TowelPattern>> = HashMap::new();
+        patterns.insert('r', vec![TowelPattern("r".to_owned())]);
         assert_eq!(1, count("r", 0, &mut patterns, &mut HashMap::new()));
 
-        let mut patterns: HashMap<char, BinaryHeap<TowelPattern>> = HashMap::new();
-        patterns.insert('r', BinaryHeap::from_iter(vec![
+        let mut patterns: HashMap<char, Vec<TowelPattern>> = HashMap::new();
+        patterns.insert('r', vec![
             TowelPattern("r".to_owned()),
             TowelPattern("rw".to_owned()),
-        ]));
-        patterns.insert('w', BinaryHeap::from_iter(vec![
+        ]);
+        patterns.insert('w', vec![
             TowelPattern("w".to_owned()),
-        ]));
+        ]);
         assert_eq!(2, count("rw", 0, &mut patterns, &mut HashMap::new()));
     }
 
