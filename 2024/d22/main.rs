f5779b8fd37001f1fb33b86124208c21e2949de9
Dan Oxlade <dan.oxlade@gmail.com>
2024-12-27 19:19:09 +0000

    2024 d22 pt2

--- 2024/d22/main.rs
+++ 2024/d22/main.rs
@@ -1,7 +1,10 @@
 use core::str;
-use std::{i64, iter::{self, Sum}, ops::{Add, Mul}, str::FromStr, time::Instant, usize};
+use std::{
+    cmp::Reverse, i64, iter::{self, Sum}, ops::Add, time::Instant
+};
 
-use aoclib::{input, timing};
+use aoclib::timing;
+use hashbrown::{HashMap, HashSet};
 use itertools::Itertools;
 
 fn main() {
@@ -13,37 +16,67 @@ fn main() {
 }
 
 fn part1(txt: &str) -> i64 {
-    let SecretNumber(total) = txt.lines().map(|l| l.parse::<i64>().unwrap()).map(SecretNumber)
+    let SecretNumber(total) = txt
+        .lines()
+        .map(|l| l.parse::<i64>().unwrap())
+        .map(SecretNumber)
         .map(|sn| sn.into_iter().nth(2000 - 1).unwrap())
         .sum();
     total
 }
 
 fn part2(txt: &str) -> i64 {
-    0
+    let mut items: HashMap<(i8, i8, i8, i8), i64> = HashMap::new();
+
+    for sn in txt
+        .lines()
+        .map(|l| l.parse::<i64>().unwrap())
+        .map(SecretNumber)
+    {
+        let mut seen: HashSet<_> = HashSet::new();
+
+        for ps in sn.price_sequence().take(2000) {
+            if seen.insert(ps.deltas.clone()) {
+                if let Some(existing) = items.get_mut(&ps.deltas) {
+                    *existing += ps.price() as i64;
+                } else {
+                    items.insert(ps.deltas.clone(), ps.price() as i64);
+                }
+            }
+        }
+    }
+
+    let (_, v) = items.iter().sorted_by_key(|(_, &v)| Reverse(v)).take(1).next().unwrap();
+    *v
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 struct SecretNumber(i64);
 
 impl SecretNumber {
-
     fn mix(&self, n: i64) -> SecretNumber {
-        // To mix a value into the secret number, 
-        // calculate the bitwise XOR of the given value and the secret number. 
-        // Then, the secret number becomes the result of that operation. 
-        // (If the secret number is 42 and you were to mix 15 into the secret number, 
-        // the secret number would become 37.)
         SecretNumber(self.0 ^ n)
     }
 
     fn prune(&self) -> SecretNumber {
-        // To prune the secret number, 
-        // calculate the value of the secret number modulo 16777216. 
-        // Then, the secret number becomes the result of that operation. 
-        // (If the secret number is 100000000 and you were to prune the secret number, 
-        // the secret number would become 16113920.)
         SecretNumber(self.0 % 16777216)
+    }
+
+    fn ones_digit(&self) -> i8 {
+        let SecretNumber(n) = self;
+        (*n % 10) as i8
+    }
+
+    fn prices(&self) -> impl Iterator<Item = i8> {
+        itertools::chain(iter::repeat_n(self.clone(), 1), self.clone().into_iter())
+            .map(|n| n.ones_digit())
+    }
+
+    fn price_sequence(&self) -> impl Iterator<Item = PriceSequence> {
+        self.prices().tuple_windows().map(|seq| PriceSequence {
+            prices: seq,
+            deltas: (seq.1 - seq.0, seq.2 - seq.1, seq.3 - seq.2, seq.4 - seq.3),
+        })
     }
 }
 
@@ -60,8 +93,6 @@ impl Sum for SecretNumber {
         iter.fold(SecretNumber(0), |acc, sn| acc + sn)
     }
 }
-
-struct SecretNumberIter(SecretNumber);
 
 impl Iterator for SecretNumber {
     type Item = SecretNumber;
@@ -81,10 +112,20 @@ impl Iterator for SecretNumber {
     }
 }
 
-#[cfg(test)]
-mod tests {    
-    use std::iter;
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct PriceSequence {
+    prices: (i8, i8, i8, i8, i8),
+    deltas: (i8, i8, i8, i8),
+}
 
+impl PriceSequence {
+    fn price(&self) -> i8 {
+        self.prices.4
+    }
+}
+
+#[cfg(test)]
+mod tests {
     use crate::*;
 
     #[test]
@@ -100,33 +141,45 @@ mod tests {
     #[test]
     fn test_123() {
         let s = SecretNumber(123);
-        let next_10: Vec<_> = s.into_iter()
-            .take(10)
-            .collect();
+        let next_10: Vec<_> = s.into_iter().take(10).collect();
 
-        assert_eq!(next_10, vec![
-            SecretNumber(15887950),
-            SecretNumber(16495136),
-            SecretNumber(527345),
-            SecretNumber(704524),
-            SecretNumber(1553684),
-            SecretNumber(12683156),
-            SecretNumber(11100544),
-            SecretNumber(12249484),
-            SecretNumber(7753432),
-            SecretNumber(5908254),
-        ])
-        
+        assert_eq!(
+            next_10,
+            vec![
+                SecretNumber(15887950),
+                SecretNumber(16495136),
+                SecretNumber(527345),
+                SecretNumber(704524),
+                SecretNumber(1553684),
+                SecretNumber(12683156),
+                SecretNumber(11100544),
+                SecretNumber(12249484),
+                SecretNumber(7753432),
+                SecretNumber(5908254),
+            ]
+        )
     }
 
     #[test]
     fn test_input_pt1() {
         let test_input = include_str!("input.test.txt");
 
-        assert_eq!(SecretNumber(8685429), SecretNumber(1).into_iter().nth(2000 - 1).unwrap());
-        assert_eq!(SecretNumber(4700978), SecretNumber(10).into_iter().nth(2000 - 1).unwrap());
-        assert_eq!(SecretNumber(15273692), SecretNumber(100).into_iter().nth(2000 - 1).unwrap());
-        assert_eq!(SecretNumber(8667524), SecretNumber(2024).into_iter().nth(2000 - 1).unwrap());
+        assert_eq!(
+            SecretNumber(8685429),
+            SecretNumber(1).into_iter().nth(2000 - 1).unwrap()
+        );
+        assert_eq!(
+            SecretNumber(4700978),
+            SecretNumber(10).into_iter().nth(2000 - 1).unwrap()
+        );
+        assert_eq!(
+            SecretNumber(15273692),
+            SecretNumber(100).into_iter().nth(2000 - 1).unwrap()
+        );
+        assert_eq!(
+            SecretNumber(8667524),
+            SecretNumber(2024).into_iter().nth(2000 - 1).unwrap()
+        );
 
         assert_eq!(37327623, part1(test_input));
     }
@@ -138,14 +191,37 @@ mod tests {
     }
 
     #[test]
+    fn test_secret_ones() {
+        assert_eq!(0, SecretNumber(10000).ones_digit());
+        assert_eq!(1, SecretNumber(10001).ones_digit());
+        assert_eq!(3, SecretNumber(3).ones_digit());
+        assert_eq!(9, SecretNumber(4565469).ones_digit());
+    }
+
+    #[test]
+    fn test_prices() {
+        let sn = SecretNumber(123);
+        let PriceSequence { prices, .. } = sn.price_sequence().next().unwrap();
+        assert_eq!(prices, (3, 0, 6, 5, 4));
+    }
+
+    #[test]
+    fn test_price_deltas() {
+        let sn = SecretNumber(123);
+        let mut itr = sn.price_sequence();
+        let PriceSequence { deltas, .. } = itr.next().unwrap();
+        assert_eq!((-3, 6, -1, -1), deltas);
+    }
+
+    #[test]
     fn test_input_pt2() {
-        let test_input = include_str!("input.test.txt");
-        assert_eq!(0, part1(test_input));
+        let test_input = "1\n2\n3\n2024";
+        assert_eq!(23, part2(test_input));
     }
 
     #[test]
     fn input_pt2() {
         let test_input = include_str!("input.txt");
-        assert_eq!(0, part2(test_input));
+        assert_eq!(2362, part2(test_input));
     }
 }
