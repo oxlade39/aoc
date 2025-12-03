9deaaf94ae75d88d2e76199d6ea5345e86edc492
Dan Oxlade <dan.oxlade@gmail.com>
2025-12-03 09:57:32 +0000

    25 d3 pt2

--- 2025/d3/main.rs
+++ 2025/d3/main.rs
@@ -15,12 +15,15 @@ fn main() {
 fn part1(txt: &str) -> usize {
     txt.lines()
         .map(|l| l.parse::<Bank>().expect("Bank"))
-        .map(|b| b.joltage())
+        .map(|b| b.joltage_n(2))
         .sum()
 }
 
-fn part2(txt: &str) -> i64 {
-    0
+fn part2(txt: &str) -> usize {
+    txt.lines()
+        .map(|l| l.parse::<Bank>().expect("Bank"))
+        .map(|b| b.joltage_n(12))
+        .sum()
 }
 
 #[derive(Debug, Clone, PartialEq, PartialOrd, Hash, Eq, Ord)]
@@ -54,33 +57,46 @@ impl FromStr for Bank {
 }
 
 impl Bank {
-    fn joltage(&self) -> usize {
-        let mut positions: [usize; 10] = [0; 10];
-        let ordered: Vec<_> = self.0.iter().sorted_by(|a, b| b.cmp(a)).collect();
-        for i in 0..self.0.len() {
-            let value = &self.0[i];
-            let current = positions[value.0];
-            if current == 0 {
-                positions[value.0] = i;
-            }                        
+    fn joltage_n(&self, n: usize) -> usize {
+        let positions: Vec<_> = self
+            .0
+            .iter()
+            .enumerate()
+            .sorted_by(|(a_pos, a), (b_pos, b)| a.cmp(b).then(b_pos.cmp(a_pos)))
+            .collect();
+
+        let mut j = 0;
+        let mut last_filled_pos_from_left = None;
+
+        for i in 0..n {
+            let mut positions = positions.clone();
+            let required_pos = n - 1 - i;
+            while let Some((position_from_left, next)) = positions.pop() {
+                let pos_from_right = self.0.len() - position_from_left;
+                if pos_from_right > required_pos {
+                    if last_filled_pos_from_left.is_none() {
+                        let num = 10_usize.pow(required_pos as u32) * next.0;
+                        j += num;
+                        last_filled_pos_from_left = Some(
+                            last_filled_pos_from_left
+                                .unwrap_or(0)
+                                .max(position_from_left),
+                        );
+                        break;
+                    } else if position_from_left > last_filled_pos_from_left.unwrap_or(0) {
+                        let num = 10_usize.pow(required_pos as u32) * next.0;
+                        j += num;
+                        last_filled_pos_from_left = Some(
+                            last_filled_pos_from_left
+                                .unwrap_or(0)
+                                .max(position_from_left),
+                        );
+                        break;
+                    }
+                }
+            }
         }
-
-        // max is last
-        let tens = if positions[ordered[0].0] == self.0.len() - 1 {
-            ordered[1]
-        } else {
-            ordered[0]
-        };
-
-        println!("tens: {:?} @ {} of {}", tens, positions[tens.0], self.0.len());
-        println!("positions: {:?}", positions);
-
-        let mut units = 0;
-        for i in (positions[tens.0] + 1)..self.0.len() {
-            units = units.max(self.0[i].0);
-        }
-
-        tens.0 * 10 + units
+        j
     }
 }
 
@@ -92,30 +108,33 @@ mod tests {
     fn test_bank_parse() {
         let s = "987654321111111";
         let b = s.parse::<Bank>();
-        assert_eq!(Ok(Bank(vec![
-            Batery(9),
-            Batery(8),
-            Batery(7),
-            Batery(6),
-            Batery(5),
-            Batery(4),
-            Batery(3),
-            Batery(2),
-            Batery(1),
-            Batery(1),
-            Batery(1),
-            Batery(1),
-            Batery(1),
-            Batery(1),
-            Batery(1),
-        ])), b);
+        assert_eq!(
+            Ok(Bank(vec![
+                Batery(9),
+                Batery(8),
+                Batery(7),
+                Batery(6),
+                Batery(5),
+                Batery(4),
+                Batery(3),
+                Batery(2),
+                Batery(1),
+                Batery(1),
+                Batery(1),
+                Batery(1),
+                Batery(1),
+                Batery(1),
+                Batery(1),
+            ])),
+            b
+        );
     }
 
     #[test]
-    fn test_bank_joltage() {
+    fn test_bank_joltage_n() {
         let s = "987654321111111";
         let b = s.parse::<Bank>().unwrap();
-        assert_eq!(98, b.joltage());
+        assert_eq!(98, b.joltage_n(2));
     }
 
     #[test]
@@ -127,18 +146,18 @@ mod tests {
     #[test]
     fn input_pt1() {
         let test_input = include_str!("input.txt");
-        assert_eq!(0, part1(test_input));
+        assert_eq!(16927, part1(test_input));
     }
 
     #[test]
     fn test_input_pt2() {
         let test_input = include_str!("input.test.txt");
-        assert_eq!(0, part1(test_input));
+        assert_eq!(3121910778619, part2(test_input));
     }
 
     #[test]
     fn input_pt2() {
         let test_input = include_str!("input.txt");
-        assert_eq!(0, part2(test_input));
+        assert_eq!(167384358365132, part2(test_input));
     }
 }
