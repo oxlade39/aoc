36aab188c07ad9c6e789e5687d03ca9a484efa26
Dan Oxlade <dan.oxlade@gmail.com>
2025-12-07 12:09:41 +0000

    25 d7 pt 2

--- 2025/d7/main.rs
+++ 2025/d7/main.rs
@@ -5,7 +5,7 @@ use aoclib::{
     grid::{FromChar, Grid, GridPosition},
     input, timing,
 };
-use hashbrown::HashSet;
+use hashbrown::{HashMap, HashSet};
 use itertools::Itertools;
 
 fn main() {
@@ -24,10 +24,18 @@ fn part1(txt: &str) -> i64 {
 }
 
 fn part2(txt: &str) -> i64 {
-    0
+    let g: Grid<Tile> = txt.parse().unwrap();
+    let (p, _start) = g.position_itr().find(|(p, t)| t == &&Tile::Start).unwrap();
+    let mut memo: HashMap<GridPosition, i64> = HashMap::new();
+    count_timelines(p, &g, &mut memo)
 }
 
-fn count_beams(p: GridPosition, g: &Grid<Tile>, count: i64, seen: &mut HashSet<GridPosition>) -> i64 {
+fn count_beams(
+    p: GridPosition,
+    g: &Grid<Tile>,
+    count: i64,
+    seen: &mut HashSet<GridPosition>,
+) -> i64 {
     for (below_p, below) in g.down_from(p).skip(1) {
         if below == &Tile::Splitter {
             let mut split_total = count;
@@ -38,14 +46,40 @@ fn count_beams(p: GridPosition, g: &Grid<Tile>, count: i64, seen: &mut HashSet<G
             if let Some((right_p, _left)) = g.right_from(below_p).skip(1).take(1).next() {
                 if seen.insert(right_p) {
                     split_total += count_beams(right_p, g, 1, seen);
-                }            
+                }
             }
 
             return split_total;
-        }        
+        }
     }
 
     count
+}
+
+fn count_timelines(p: GridPosition, g: &Grid<Tile>, memo: &mut HashMap<GridPosition, i64>) -> i64 {
+    if let Some(existing) = memo.get(&p) {
+        *existing
+    } else {
+        for (below_p, below) in g.down_from(p).skip(1) {
+            if below == &Tile::Splitter {
+                let mut result = 0;
+                if let Some((left_p, _left)) = g.left_from(below_p).skip(1).take(1).next() {
+                    let timelines = count_timelines(left_p, g, memo);
+                    memo.insert(left_p, timelines);
+                    result += timelines;
+                }
+                if let Some((right_p, _right)) = g.right_from(below_p).skip(1).take(1).next() {
+                    let timelines = count_timelines(right_p, g, memo);
+                    memo.insert(right_p, timelines);
+                    result += timelines;
+                }
+                memo.insert(below_p, result);
+                return result;
+            }
+        }
+        memo.insert(p, 1);
+        1
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -81,18 +115,18 @@ mod tests {
     #[test]
     fn input_pt1() {
         let test_input = include_str!("input.txt");
-        assert_eq!(0, part1(test_input));
+        assert_eq!(1585, part1(test_input));
     }
 
     #[test]
     fn test_input_pt2() {
         let test_input = include_str!("input.test.txt");
-        assert_eq!(0, part1(test_input));
+        assert_eq!(40, part2(test_input));
     }
 
     #[test]
     fn input_pt2() {
         let test_input = include_str!("input.txt");
-        assert_eq!(0, part2(test_input));
+        assert_eq!(16716444407407, part2(test_input));
     }
 }
